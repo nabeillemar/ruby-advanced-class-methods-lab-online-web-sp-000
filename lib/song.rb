7da62dbef740e3d2320b6143084f9bca52405eea
Nabeil Lemar <nabeillemar@gmail.com>
2020-11-07 04:34:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,6 +24,33 @@ end
 
 def self.create_by_name(name)
   song = self.new 
+  song.name = name 
+  @@all << song 
+end 
+
+
+def self.find_by_name(name)
+  @@all.find{|x| x.name == name}
+end 
+
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+end 
+
+def self.alphabetical
+  @@all.sort_by{|x| x.name} 
+end 
+
+def self.new_from_filename(name)
+  song = self.new
+  song.name = (name.split(" - ")[1].chomp(".mp3"))
   
+end 
+
+def self.create_from_filename
+end 
+
+def self.destory_all
+end 
 
 end
